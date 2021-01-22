e5e4749ad91f61f9dde2b17586c48948da2d44bf
Rabbi Alam <frabbi.alam@gmail.com>
2021-01-22 12:09:34 +0100

    commiting chatbot

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,48 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+    </v-app-bar>
+      
+
+     
+
+    
+
+    <v-main>
+      <div class="d-flex align-center">
+       <amplify-chatbot :chatbotConfig="chatbotConfig"></amplify-chatbot>
+      </div>
+    </v-main>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-#nav {
-  padding: 30px;
+import {Interactions} from 'aws-amplify';
+export default {
+  name: 'App',
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+  data: () => ({
+    chatbotConfig: {
+      bot:"OrderFlowers_dev",
+      clearComplete: false
+    }
+  }),
+  mounted() {
+    Interactions.onComplete("OrderFlowers_dev", this.handelComplete)
+  },
+  methods: {
+    handelComplete(error, confirmation) {
+      if(error) {
+        alert(error);
+        return
+      }
+      alert(JSON.stringify(confirmation));
     }
   }
-}
-</style>
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Amplify, * as AmplifyModules from 'aws-amplify';
+import {AmplifyPlugin} from 'aws-amplify-vue';
+import awsConfig from './aws-exports';
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify';
+
+
+Amplify.configure(awsConfig);
+Vue.use(AmplifyPlugin, AmplifyModules);
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
